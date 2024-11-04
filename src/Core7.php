1e9738f8eecea5b7d67b656c3f17113e63ee554e
CLOUDFRAMEWORK DEVOPS <devops@cloudframework.io>
2024-11-04 09:00:09 +0100

    Upgrade to 8.3.34

--- src/Core7.php
+++ src/Core7.php
@@ -168,7 +168,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = '8.3.33';  // 2024-10-24
+        var $_version = '8.3.34';  // 2024-11-04
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
