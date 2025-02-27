740603618e97d53297769506df399330bc4af3c0
CLOUDFRAMEWORK DEVOPS <devops@cloudframework.io>
2025-02-27 18:46:56 +0100

    Update Core7 version to 8.3.40
    Bump Core7 version from 8.3.39 to 8.3.40 in Core7.php and composer-dist.json. This ensures compatibility and reflects the latest updates in the backend-core-php8 framework.

--- src/Core7.php
+++ src/Core7.php
@@ -168,7 +168,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = '8.3.39';  // 2025-02-27
+        var $_version = '8.3.40';  // 2025-02-27
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
