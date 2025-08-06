252251388d0af503c830a2ead4d992b5eb8f7234
Adrian at CF <am@cloudframework.io>
2025-08-06 16:01:50 +0200

    Bump Core7 to version 8.3.57 in Core7.php and composer-dist.json

--- src/Core7.php
+++ src/Core7.php
@@ -168,7 +168,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = '8.3.56';  // 2025-08-06
+        var $_version = '8.3.57';  // 2025-08-06
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
