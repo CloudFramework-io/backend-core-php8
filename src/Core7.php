04d87329c5fbd8f2c31394247ebd124d92878430
Adrian at CF <am@cloudframework.io>
2025-08-06 15:55:46 +0200

    Update Core7 to version 8.3.56 and refine log entity handling
    Bumped Core7 version to 8.3.56 and updated `createEntities` method to handle and store the first entity more robustly. This enhances reliability when managing log entities.

--- src/Core7.php
+++ src/Core7.php
@@ -168,7 +168,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = '8.3.55';  // 2025-07-27
+        var $_version = '8.3.56';  // 2025-08-06
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
@@ -7958,7 +7958,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
             }
 
             //region $this->dsLogs->createEntities($entity)
-            $this->dsLogs->createEntities($entity);
+            $entity = $this->dsLogs->createEntities($entity)[0]??null;
             if ($this->dsLogs->error) {
                 $this->addError($this->dsLogs->errorMsg);
                 $this->core->logs->add(['data'=>$entity,'error'=>$this->dsLogs->errorMsg],'CFILog_add_error');
