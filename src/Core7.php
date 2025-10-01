281d525f4372b371368dff96fccc583bc150c075
CLOUDFRAMEWORK DEVOPS <devops@cloudframework.io>
2025-10-01 09:21:19 +0200

    Update backend-core-php8 to version 8.3.62
    Bumped the dependency in composer-dist.json to 8.3.62 and updated the Core7 version string to align with the latest release.

--- src/Core7.php
+++ src/Core7.php
@@ -168,7 +168,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = '8.3.61';  // 2025-10-01 1
+        var $_version = '8.3.62';  // 2025-10-01 2
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
