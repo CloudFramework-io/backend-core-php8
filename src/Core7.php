fd45ca370f737fbe83ffcfc2bc9fa1c57b9fb79f
CLOUDFRAMEWORK DEVOPS <devops@cloudframework.io>
2025-12-15 11:12:52 +0100

    [task:6578636072681472] Bump `backend-core-php8` version to 8.4.17 and update dependency in `composer-dist.json`.

--- src/Core7.php
+++ src/Core7.php
@@ -168,7 +168,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = '8.4.16';  // 2025-12-06 2
+        var $_version = '8.4.17';  // 2025-12-15 1
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
