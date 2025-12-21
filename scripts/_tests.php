e2542f0897250e3d613526cbbb6e42c2943f7d50
CLOUDFRAMEWORK DEVOPS <devops@cloudframework.io>
2025-12-21 23:14:58 +0100

    [task:6578636072681472] Update test script paths in `_tests.php` to reference `/_tests/` directory in `backend-core-php8`

--- scripts/_tests.php
+++ scripts/_tests.php
@@ -3,7 +3,7 @@
  * Router to include tests scripts of CLOUDFRAMEWORK
  */
 $test_file = ($script[1]??null)?:'default';
-if(!is_file(__DIR__."/tests/{$test_file}.php")) {
+if(!is_file(__DIR__."/_tests/{$test_file}.php")) {
     die("Test [{$test_file}] does not exists");
 }
-include_once(__DIR__."/tests/{$test_file}.php");
+include_once(__DIR__."/_tests/{$test_file}.php");
