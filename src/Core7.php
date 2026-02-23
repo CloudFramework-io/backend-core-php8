c3857363119d56727c19a7bb9257b878ce1f6b39
CLOUDFRAMEWORK DEVOPS <devops@cloudframework.io>
2026-02-23 08:15:38 +0100

    Bump version to 8.4.38

--- src/Core7.php
+++ src/Core7.php
@@ -168,7 +168,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = '8.4.37';  // 2026-02-22
+        var $_version = '8.4.38';  // 2026-02-23
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
